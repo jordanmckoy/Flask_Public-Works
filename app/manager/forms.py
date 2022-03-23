5d67b0eedb1321cf28543966725831b669d68e69
TypicalTropic <jordan.bmp.mckoy@gmail.com>
2022-03-23 10:29:01 -0500

    Changed location of complaint form

--- app/manager/forms.py
+++ app/manager/forms.py
@@ -79,38 +79,3 @@ class PromoteRegularForm(FlaskForm):
 class PromoteTemporaryForm(FlaskForm):
     hr_wage = DecimalField('hr_wage', validators=[DataRequired()])
     contract = StringField('contract', validators=[DataRequired()])
-
-# complaint form
-
-
-class ComplaintForm(FlaskForm):
-    email_address = StringField('Email',
-                      id='email_create',
-                      validators=[DataRequired(), Email()])
-
-    first_name = StringField(
-        'first name', id='first_name_create', validators=[DataRequired()])
-
-    last_name = StringField('Last Name', id='last_name_create',
-                            validators=[DataRequired()])
-
-    street_number = StringField(
-        'street number', id='street_number_create')
-
-    street_name = StringField(
-        'street name', id='street_name_create', validators=[DataRequired()])
-
-    city = StringField('city', id='city_create', validators=[DataRequired()])
-
-    phone = IntegerField('phone', id='phone_create',
-                         validators=[DataRequired()])
-
-    parish = StringField('parish', id='last_name_create',
-                         validators=[DataRequired()])
-
-    job_id = StringField(label='Job ID', validators=[DataRequired()])
-
-    date = DateField('date', validators=[DataRequired()])
-
-    complaint = StringField(label='Complaint Content',
-                            validators=[DataRequired()])
